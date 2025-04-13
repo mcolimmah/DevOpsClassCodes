8903ef302e36839c1717e3e652de86090f9a0dc9
mcolimmah <157446382+mcolimmah@users.noreply.github.com>
2025-04-13 01:06:32 -0500

    Update ContactForm.java
    Changed it back to Last name and Mobile Number

--- src/main/java/com/edurekademo/tutorial/addressbook/ContactForm.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/ContactForm.java
@@ -26,8 +26,8 @@ public class ContactForm extends FormLayout {
     Button save = new Button("Save", this::save);
     Button cancel = new Button("Cancel", this::cancel);
     TextField firstName = new TextField("First Name");
-    TextField lastName = new TextField("Surname Name");
-    TextField phone = new TextField("Mobile Numbers");
+    TextField lastName = new TextField("Last Name");
+    TextField phone = new TextField("Mobile Number");
     TextField email = new TextField("Email Add");
     DateField birthDate = new DateField("Birth Date");
   
